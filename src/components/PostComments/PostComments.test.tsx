2b795ed826983024ae2b882bf7ddb5a6e0425930
Reinaldo <reinaldo18junior@gmail.com>
2024-10-04 15:02:35 -0300

    Instalando Jest e testando a inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    test('Adicionar dois cometários', () => {
+        render(<PostComment />)
+
+        fireEvent.change(screen.getByTestId('escrever-comentario'), {
+            target: {
+                value: 'Primeiro comentário do teste',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-add-comentario'));
+
+        fireEvent.change(screen.getByTestId('escrever-comentario'), {
+            target: {
+                value: 'Segundo comentário do teste',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-add-comentario'));
+
+        expect(screen.getAllByTestId('area-do-comentario')).toHaveLength(2);
+    });
+}); 
